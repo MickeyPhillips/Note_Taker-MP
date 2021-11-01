47c5861628cf233145ccd7aaf25a0081392a23f5
Mickey Phillips <phillipsmickey41@gmail.com>
2021-10-31 20:14:09 -0500

    routes created

---
+++ routes/apiRoutes/noteRoutes.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { createNewNote, validateNote } = require('../../lib/notes');
+const { notes } = require('../../db/db.json');
+
+router.get('/notes', (req, res) => {
+    let results = notes;
+    res.json(results);
+})
+
+router.post('/notes', (req, res) => {
+    req.body.id = notes.length.toString();
+
+    if(!validateNote(req.body)) {
+        res.status(400).send('The Note is not properly formatted.')
+    }
+    else {
+        const note = createNewNote(req.body, notes);
+        res.json(note)
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const apiRoutes = require('./routes/apiRoutes')
+const htmlRoutes = require('./routes/htmlRoutes')
 
 const PORT = process.env.PORT || 3002;
 const app = express();
