9e3e40b7d287ee2a18bcfec709f83f5b86cd4155
Mickey Phillips <phillipsmickey41@gmail.com>
2021-10-31 19:27:02 -0500

    filed structure added

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3002;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'))
+
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes)
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
