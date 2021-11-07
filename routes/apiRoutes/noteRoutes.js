d37cd58688699966d42fd5b2f25dd938c9e2d253
Mickey Phillips <phillipsmickey41@gmail.com>
2021-11-07 11:58:19 -0600

    delete added

--- routes/apiRoutes/noteRoutes.js
+++ routes/apiRoutes/noteRoutes.js
@@ -1,14 +1,14 @@
 const router = require('express').Router();
-const { createNewNote, validateNote } = require('../../lib/notes');
+const { createNewNote, validateNote, findNote, deleteNote } = require('../../lib/notes');
 const { notes } = require('../../db/db.json');
+const { v1: uuidv1, v4: uuidv4 } = require('uuid') 
 
 router.get('/notes', (req, res) => {
-    let results = notes;
-    res.json(results);
+    res.json(notes);
 })
 
 router.post('/notes', (req, res) => {
-    req.body.id = notes.length.toString();
+    req.body.id = uuidv4();
 
     if(!validateNote(req.body)) {
         res.status(400).send('The Note is not properly formatted.')
@@ -17,6 +17,12 @@ router.post('/notes', (req, res) => {
         const note = createNewNote(req.body, notes);
         res.json(note)
     }
+})
+
+router.delete('/notes/:id', (req, res) => {
+    const result = findNote(req.params.id, notes);
+    deleteNote(result, notes);
+    res.json(notes);
 })
 
 module.exports = router;
